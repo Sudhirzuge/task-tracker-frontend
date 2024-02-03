305587e0939c814bdcd23f15abbb5a0383bbf892
“Sudhirzuge” <sudhirzuge2000@gmail.com>
2024-02-03 20:30:47 +0530

    " frontend completed using react "

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListTaskComponent from "./components/ListTaskComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TaskComponent from "./components/TaskComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<ListTaskComponent />}></Route>
+          <Route path="/tasks" element={<ListTaskComponent />}></Route>
+          <Route path="/add-Task" element={<TaskComponent />}></Route>
+          <Route path="/edit-task/:id" element={<TaskComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// 127.0.0.1:3000 = localhost:3000
